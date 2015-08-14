e74c421efc6bb623a4fe485b7d9b2d4acfe6f558
Andy Norris <RuralQuants@bitbucket.org>
2015-08-14 11:22:36 +0100

    Added more read-related methods to FS_DT_USARTDriver_t

--- inc/FS_DT_USART.h
+++ inc/FS_DT_USART.h
@@ -6,8 +6,10 @@ typedef struct
   uint16_t(*writeBytes)(const char * bytes, uint16_t numBytes);
   uint16_t(*writeLine)(const char * line);
 
+  uint16_t(*rxBytesAvailable)(void);
   uint16_t(*readBytes)(const char * buf, uint16_t numBytes);
   uint16_t(*readLine)(const char * buf);
+  uint16_t(*readLineTruncate)(const char * buf, uint16_t maxLen);
 
 }FS_DT_USARTDriver_t;
 
