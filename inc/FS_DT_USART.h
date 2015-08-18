06af14df7c5010c10e276a5103cdf593ad718ccc
Andy Norris <RuralQuants@bitbucket.org>
2015-08-18 08:41:48 +0100

    Modified USART driver interface type with new methods

--- inc/FS_DT_USART.h
+++ inc/FS_DT_USART.h
@@ -7,9 +7,9 @@ typedef struct
   uint16_t(*writeLine)(const char * line);
 
   uint16_t(*rxBytesAvailable)(void);
-  uint16_t(*readBytes)(const char * buf, uint16_t numBytes);
-  uint16_t(*readLine)(const char * buf);
-  uint16_t(*readLineTruncate)(const char * buf, uint16_t maxLen);
+  uint16_t(*readBytes)(unsigned char * buf, uint16_t numBytes);
+  uint16_t(*readLine)(unsigned char * buf);
+  uint16_t(*readLineTruncate)(unsigned char * buf, uint16_t maxLen);
 
 }FS_DT_USARTDriver_t;
 
